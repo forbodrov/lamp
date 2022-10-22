a87af2ea09cad03278bb7297ce51f1cec2b3579f
Numerator <123@123.ru>
2022-10-22 12:40:06 +0300

    terraform-homework

---
+++ terraform/instance.tf
@@ -0,0 +1,54 @@
+data "vkcs_compute_flavor" "compute" {
+  # name = "Basic-1-2-20"
+  name = "Standard-2-2-20"
+}
+
+data "vkcs_images_image" "compute" {
+  # name = "Ubuntu-18.04-Standard"
+  name = "Ubuntu-22.04-Standard"
+}
+
+resource "vkcs_compute_instance" "compute" {
+  name                    = "user07-homework"
+  flavor_id               = data.vkcs_compute_flavor.compute.id
+  security_groups         = ["default","all"]
+  availability_zone       = "GZ1"
+  key_pair = vkcs_compute_keypair.test-keypair.name
+
+  block_device {
+    uuid                  = data.vkcs_images_image.compute.id
+    source_type           = "image"
+    destination_type      = "volume"
+    volume_type           = "ceph-ssd"
+    volume_size           = 8
+    boot_index            = 0
+    delete_on_termination = true
+  }
+
+  network {
+    uuid = vkcs_networking_network.compute.id
+  }
+
+  depends_on = [
+    vkcs_networking_network.compute,
+    vkcs_networking_subnet.compute
+  ]
+}
+
+resource "vkcs_compute_keypair" "test-keypair" {
+  name       = "user07-homework"
+  public_key = file("../keys/id_rsa.pub")
+}
+
+resource "vkcs_networking_floatingip" "fip" {
+  pool = data.vkcs_networking_network.extnet.name
+}
+
+resource "vkcs_compute_floatingip_associate" "fip" {
+  floating_ip = vkcs_networking_floatingip.fip.address
+  instance_id = vkcs_compute_instance.compute.id
+}
+
+output "instance_fip" {
+  value = vkcs_networking_floatingip.fip.address
+}
